e91bb79f6c1e1553fe59ba57d7b03926c2bb48d9
woong-e <ska2748@naver.com>
2021-10-26 22:03:57 +0900

    Updates

--- src/components/common/PageHeader/pageHeader.tsx
+++
@@ -1,8 +0,0 @@
-import React, {memo} from 'react';
-import { ComponentTitleWrapperStyle } from './pageHeader.style';
-
-const PageTitle = memo((props) =>
-	<ComponentTitleWrapperStyle className="teoComponentTitle">{props.children}</ComponentTitleWrapperStyle>
-)
-
-export default PageTitle
--- src/helpers/utility.ts
+++
@@ -1,9 +0,0 @@
-export const getFirstUrl = (url: string) => {
-	const urlSplit = url.split('/');
-	return urlSplit[urlSplit.length -1];
-}
-
-export const getMiddleUrl = (url: string) => {
-	const urlSplit = url.split('/');
-	return urlSplit[urlSplit.length -2];
-}
